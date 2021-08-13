6426031f9abec88ae33e9845ad7be36f15fee288
Marius <72674162+rcerber@users.noreply.github.com>
2021-08-13 07:47:20 +0200

    13.08

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
+func main() {
 	fmt.Println("Hello world!")
+	fmt.Println("The time is", time.Now())
+	// panic("asdasd")
+	fmt.Println("Bye world!")
 }
