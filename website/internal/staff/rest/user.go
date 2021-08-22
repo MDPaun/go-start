b0a5c602a70cd0de430b59ef020ab6af431f1507
Marius <72674162+rcerber@users.noreply.github.com>
2021-08-22 09:38:10 +0200

    22.08

--- website/internal/staff/rest/user.go
+++ website/internal/staff/rest/user.go
@@ -1,62 +1,62 @@
 package rest
 
-import (
-	"io"
-	"net/http"
-	"strconv"
+// import (
+// 	"io"
+// 	"net/http"
+// 	"strconv"
 
-	"github.com/MDPaun/go-start/tree/main/website/internal/staff/service"
+// 	"github.com/MDPaun/go-start/tree/main/website/internal/staff/service"
 
-	"github.com/labstack/echo/v4"
-)
+// 	"github.com/labstack/echo/v4"
+// )
 
-// //CreatePet route for POST
-// func CreatePet(c echo.Context) error {
-// 	fmt.Println("Creating Pet")
-// 	p := new(db.Pet)
-// 	// bind request body to the model object
-// 	if err := c.Bind(p); err != nil {
-// 		panic(err)
-// 	}
-// 	err:=service.CreatePet(p)
-// 	if err!=nil{
-// 		panic(err)
-// 	}
-// 	return c.String(http.StatusCreated, "Pet created successfully")
-// }
+// // //CreatePet route for POST
+// // func CreatePet(c echo.Context) error {
+// // 	fmt.Println("Creating Pet")
+// // 	p := new(db.Pet)
+// // 	// bind request body to the model object
+// // 	if err := c.Bind(p); err != nil {
+// // 		panic(err)
+// // 	}
+// // 	err:=service.CreatePet(p)
+// // 	if err!=nil{
+// // 		panic(err)
+// // 	}
+// // 	return c.String(http.StatusCreated, "Pet created successfully")
+// // }
 
-// //GetPetByCategory route for GET
-// func GetPetByCategory(c echo.Context) error {
-// 	id, _ := strconv.Atoi(c.QueryParam("category_id"))
-// 	response, err := service.GetPetByCategory(id)
+// // //GetPetByCategory route for GET
+// // func GetPetByCategory(c echo.Context) error {
+// // 	id, _ := strconv.Atoi(c.QueryParam("category_id"))
+// // 	response, err := service.GetPetByCategory(id)
+// // 	if err != nil {
+// // 		panic(err)
+// // 	}
+// // 	return c.JSON(http.StatusOK, response)
+
+// // }
+
+// //GetStaff route for GET
+// func GetStaff1(c echo.Context) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
+// 	response, err := service.GetStaff(id)
 // 	if err != nil {
 // 		panic(err)
 // 	}
 // 	return c.JSON(http.StatusOK, response)
-
 // }
 
-//GetStaff route for GET
-func GetStaff1(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	response, err := service.GetStaff(id)
-	if err != nil {
-		panic(err)
-	}
-	return c.JSON(http.StatusOK, response)
-}
-
-func GetStaff(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "cat cat cat")
-}
-
-// //DeletePet route for DELETE
-// func DeletePet(c echo.Context) error {
-// 	id, _ := strconv.Atoi(c.Param("id"))
-// 	err := service.DeletePet(id)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return c.JSON(http.StatusOK, "Deleted")
-
+// func GetStaff(res http.ResponseWriter, req *http.Request) {
+// 	io.WriteString(res, "cat cat cat")
 // }
+
+// // //DeletePet route for DELETE
+// // func DeletePet(c echo.Context) error {
+// // 	id, _ := strconv.Atoi(c.Param("id"))
+// // 	err := service.DeletePet(id)
+// // 	if err != nil {
+// // 		panic(err)
+// // 	}
+// // 	return c.JSON(http.StatusOK, "Deleted")
+
+// // }
